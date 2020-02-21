5a1f531816f3fb7ab535c63a28cecc86d0ee9e75
thayne-allen <59717714+thayne-allen@users.noreply.github.com>
2020-02-20 19:10:45 -0700

    initial commit

--- index.js
+++ index.js
@@ -41,7 +41,23 @@ class Airplane {
 */
 
 class Person {
-
+    constructor(attributes) {
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.stomach = [];
+  }
+  //methods go here
+  eat(someFood) {
+    if (this.stomach.length < 10)
+    this.stomach.push(someFood);
+  }
+  poop() {
+    if (this.stomach.length >= 10)
+      this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -59,7 +75,25 @@ class Person {
 */
 
 class Car {
-
+  constructor(attributes) {
+    this.model = attributes.model;
+    this.milesPerGallon = attributes.milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }//methods go here
+  fill(gallons) {
+    this.tank = this.tank + gallons;
+  }
+  drive(distance) {
+    if (this.tank - this.milesPerGallon < distance) {
+      this.odometer = distance;
+      this.tank = this.tank - distance / this.milesPerGallon;
+    } else {
+      this.odometer = this.milesPerGallon - this.tank;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  };
 }
 
 /*
@@ -75,7 +109,15 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  //methods
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -92,8 +134,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(attributes) {
+    super(attributes)
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }//methods go here
+  demo(subject) {
+    return `Today we are learning about ${subject}`
+  }
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`
+  }
 }
 
 /*
@@ -111,7 +164,23 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(attributes) {
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+
+  }//methods go here
+  listSubjects() {
+    return `Loving ${this.favSubjects}`
+  }
+  PRAssignment(student,subject) {
+    return `${student.name} has submitted a PR for ${subject}`
+  }
+  sprintChallenge(student, subject) {
+    return `${student.name} has begun sprint challenge on ${subject}`
+  }
 
 }
 
@@ -128,8 +197,18 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor{
+  constructor(attributes) {
+    super(attributes);
+    this.gradClassName = attributes.gradClassName;
+    this.favInstructor = attributes.favInstructor;
+  }//methods
+  standUp(channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!`
+  }
+  debugsCode(student,subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
 }
 
 /*
